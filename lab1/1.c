5cba995ccb86beb163d004cecd3bdef6cfc402ac
your <you@example.com>
2019-11-08 06:24:50 -0800

    c to f program version1

--- lab1/1.c
+++ lab1/1.c
@@ -1,26 +1,17 @@
 #include<stdio.h>
-#include<stdio.h>
-int count_one_bits(unsigned int value)
-{
-    int count=0;
-    while (value !=0)
-    {
-        if (value%2==1)
-        {
-            count++;
-        }
-        value=value>>1;
-    }
-    return count;
-}
 int main()
 {
-    int num;
-    int ret;
-    printf("请输入一个大于0的数\n");
-    scanf("%d",&num);
-    ret=count_one_bits(num);
-    printf("%d",ret);
-    system("pause");
+    int count=0;
+    int a=0;
+    int b=0;
+    scanf("%d",&b);
+    while(b!=0)
+    {
+        a=b%2;
+        b=b/2;
+        if(a==1)
+        count++;
+    }
+    printf("%d\n",count);
     return 0;
 }
